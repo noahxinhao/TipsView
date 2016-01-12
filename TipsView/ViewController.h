2c4489680c1c9c91b42711e8fa93c59bbc9e038b
笑你妹c <ytx0573@sina.com>
2016-01-12 17:55:10 +0800

    Initial Commit

---
+++ TipsView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipsView
+//
+//  Created by Johnson on 1/12/16.
+//  Copyright © 2016 Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipsView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TipsView
+//
+//  Created by Johnson on 1/12/16.
+//  Copyright © 2016 Johnson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
